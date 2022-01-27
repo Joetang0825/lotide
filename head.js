76b0bbc2c57dc0ec7f14d72687a245da690201ad
Joe Tang <joetang81@gmail.com>
2022-01-27 15:14:58 -0500

    head.js

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(128568)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(128566)} Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const head = function(Array) {
+  return Array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
