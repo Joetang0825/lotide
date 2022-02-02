5c807c7ee0dfac5b4ad0d30d855d55e449692ee3
Joe Tang <joetang81@gmail.com>
2022-02-02 14:42:12 -0500

    countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(128568)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(128566)} Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const result = {};
+
+  for (char of sentence) {
+    if (char !== ' ') {
+      if (result[char]) {
+        result[char]++;
+      }
+      else {
+        result[char] = 1;
+      }
+    }
+  }
+
+  return result;
+}
+
+/*
+console.log(countLetters('LH L '));
+const result1 = countLetters("lighthouse in the house");
+
+for (const element in result1) {
+  console.log (element, result1[element]);
+}*/
