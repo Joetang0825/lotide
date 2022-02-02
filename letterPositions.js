4a7cd7f44398a6244632d99479a3d80584cdf24e
Joe Tang <joetang81@gmail.com>
2022-02-02 16:08:04 -0500

    letterPosition.js

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = function (array1, array2) {
+  let check = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      check = false;
+    }
+  }
+  return check;
+}
+
+const assertArraysEqual = function (ar1, ar2) {
+  if (eqArrays(ar1, ar2)) {
+    console.log(`${String.fromCodePoint(128568)} Assertion Passed: ${ar1} === ${ar2}`);
+  } else {
+    console.log(`${String.fromCodePoint(128566)} Assertion Failed: ${ar1} != ${ar2}`);
+  }
+}
+
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      }
+      else {
+        results[sentence[i]] = [];
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").l, [2,3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
+
+
+let results2 = (letterPositions("lighthouse in the house"));
+for (let x in results2){
+  console.log (`{x}: ${results2[x]}`);
+}
